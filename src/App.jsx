4ccaa157716d62a98f54eb575eaee540994366b3
aarushi353 <aarushiabrol999@gmail.com>
2021-05-10 21:25:31 +0530

    part 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "./notes";
 
 function App(){
    return <div>
              <Header />
-             <Note />
+             {notes.map((note)=>{
+           return <Note
+           title={note.title}
+           about={note.content}
+
+          />
+   
+})}
              <Footer />
           </div>
 }
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Note(){
+function Note(props){
     return <div className="note">
-        <h1>For the title</h1>
-        <p>For the content</p>
+        <h1>{props.title}</h1>
+        <p>{props.about}</p>
     </div>
 }
 
