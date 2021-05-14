5d0fdf7afbe9ae24574da89490e1c20c32139694
aarushi353 <aarushiabrol999@gmail.com>
2021-05-15 02:17:13 +0530

    deleting feature added

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,26 @@ function App(){
       return [...prevNotes, newNote];
    });
    }
+   function deleteNote(id){
+   setNotes(prevNotes=>{
+      return prevNotes.filter((noteItem, index)=>{
+      return index !== id;
+      })
+   })
+   }
    return (<div>
              <Header />
              <CreateArea 
                 onAdd={addNote} />
-                {notes.map((noteItem)=>{
-                   return <Note 
+                {notes.map((noteItem, index)=>{
+                   return (<Note 
+                   key={index}
+                   id={index}
                    title={noteItem.title}
-                   content={noteItem.content} />
+                   content={noteItem.content} 
+                   onDelete={deleteNote}   
+                   />
+                );
                 })}
 
             
