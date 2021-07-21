5762095abd5707593455898504e220ae20520d8c
aarushi353 <aarushiabrol999@gmail.com>
2021-07-22 00:22:33 +0530

    Updates

---
+++ precache-manifest.378b0ca4c6f6d4e650edd6f9123a1d1b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19bce0ac7858c57962e8c36e6f42d73",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "d79a9b258dc97df7d42d",
+    "url": "/Keeper/static/js/2.3470c36c.chunk.js"
+  },
+  {
+    "revision": "8494d53a6db36b7399c1",
+    "url": "/Keeper/static/js/main.c653191c.chunk.js"
+  },
+  {
+    "revision": "c2cd02bec1164cebc775",
+    "url": "/Keeper/static/js/runtime-main.8d08ef39.js"
+  }
+]);
